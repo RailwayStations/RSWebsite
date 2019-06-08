0e74a2048f805f958db030fc1c6b554699e664db
Peter Storch <peter.storch@gmail.com>
2019-06-08 23:48:56 +0200

    fix #41 improve user registration

--- map/js/settings.js
+++ map/js/settings.js
@@ -1,8 +1,9 @@
 /*jslint browser: true*/
 /*global $,L*/
 //-----------------------------------------------------------------------
+var loggedIn = false;
 
-function register(userProfile) {
+function register(userProfile, uploadTokenOnly) {
 	"use strict";
 
   console.log(JSON.stringify(userProfile));
@@ -17,13 +18,90 @@ function register(userProfile) {
     });
 
   request.done(function (data) {
-    setResultMessage('Registrierung erfolgreich');
-			});
+		if (uploadTokenOnly) {
+			setResultMessage('Upload-Token angefordert, bitte schaue in Deine Email');
+		} else {
+    	setResultMessage('Registrierung erfolgreich');
+		}
+	});
 
 	request.fail(function (jqXHR, textStatus, errorThrown) {
-    setResultMessage('Registrierung fehlgeschlagen: ' + textStatus + ', ' + errorThrown);
-			});
+   	setResultMessage('Fehler: ' + textStatus + ', ' + errorThrown);
+	});
 
+}
+
+function onRequestUploadToken() {
+	"use strict";
+
+	var userProfile = getUserProfileForm();
+	if (!userProfile.email || userProfile.email.length == 0) {
+		setResultMessage('Bitte Email angeben, um einen neune Upload-Token zu bekommen.');
+		return false;
+	}
+
+	register(userProfile, true);
+	return false;
+}
+
+function onLogin() {
+	"use strict";
+
+	var userProfile = getUserProfileForm();
+	if (!userProfile.email || userProfile.email.length == 0
+		|| !userProfile.uploadToken || userProfile.uploadToken.length == 0) {
+		setResultMessage('Bitte Email und Upload-Token zum Login angeben');
+		return false;
+	}
+	login(userProfile);
+	return false;
+}
+
+function login(userProfile, quiet) {
+	"use strict";
+
+	if (!userProfile.email || userProfile.email.length == 0
+		|| !userProfile.uploadToken || userProfile.uploadToken.length == 0) {
+		console.log("Not logged on");
+		return;
+	}
+
+	var request = $.ajax({
+			url: getAPIURI() + "/myProfile",
+      contentType: "application/json; charset=utf-8",
+			type: "GET",
+			dataType: "json",
+      processData: true,
+			headers: {"Upload-Token":userProfile.uploadToken,
+					"Email": userProfile.email
+			},
+      data: JSON.stringify(userProfile)
+    });
+
+  request.done(function (data) {
+    loggedIn = true;
+		data.uploadToken = userProfile.uploadToken;
+		data.cc0 = data.license.startsWith("CC0");
+		setUserProfile(data);
+		setUserProfileForm(data);
+		if (!quiet) {
+			setResultMessage('Login erfolgreich.');
+		}
+	});
+
+	request.fail(function (jqXHR, textStatus, errorThrown) {
+		badUploadToken(userProfile);
+	});
+}
+
+function badUploadToken(userProfile) {
+	"use strict";
+
+	loggedIn = false;
+	userProfile.uploadToken = "";
+	setUserProfile(userProfile);
+	setUserProfileForm(userProfile);
+	setResultMessage('Upload-Token ungültig, bitte einen Neuen anfordern');
 }
 
 function togglePoints() {
@@ -34,7 +112,9 @@ function togglePoints() {
 	localStorage.setItem("showPoints", showPoints ? "true" : "false");
 }
 
-function saveProfile() {
+function getUserProfileForm() {
+	"use strict";
+
   var userProfile = getUserProfile();
   userProfile.nickname = $("#profileNickname").val();
   userProfile.email = $("#profileEmail").val();
@@ -48,33 +128,88 @@ function saveProfile() {
   } else {
     userProfile.license = "";
   }
+
+	return userProfile;
+}
+
+function uploadProfile(userProfile) {
+	"use strict";
+
+	var request = $.ajax({
+			url: getAPIURI() + "/myProfile",
+      contentType: "application/json; charset=utf-8",
+			type: "POST",
+			data: JSON.stringify(userProfile),
+			dataType: "text",
+      processData: false,
+			headers: {"Upload-Token":userProfile.uploadToken,
+					"Email": userProfile.email
+			},
+      data: JSON.stringify(userProfile)
+    });
+
+  request.done(function (data) {
+    loggedIn = true;
+		setResultMessage('Profil gespeichert');
+	});
+
+	request.fail(function (jqXHR, textStatus, errorThrown) {
+		var status = jqXHR.status;
+		if (status == 400) {
+			setResultMessage('Ungültige Daten: ' + textStatus + ', ' + errorThrown);
+		} else if (status == 401) {
+			setResultMessage('Ungültige Token, bitte einen Neuen anfordern');
+			badUploadToken(userProfile);
+		} else if (status == 409) {
+			setResultMessage('Profile kann nicht gespeichert werden, es liegt ein Konflikt mit einem anderen Nickname oder einer Email vor.');
+		} else {
+			setResultMessage('Speichern fehlgeschlagen: ' + textStatus + ', ' + errorThrown);
+		}
+	});
+
+}
+
+function saveProfile() {
+	"use strict";
+
+  var userProfile = getUserProfileForm();
   setUserProfile(userProfile);
   if (userProfile.uploadToken === undefined || userProfile.uploadToken.length == 0) {
-    register(userProfile);
+  	register(userProfile);
   } else {
-    setResultMessage('Benutzerprofil gespeichert');
+		if (loggedIn) {
+			uploadProfile(userProfile);
+		} else {
+    	setResultMessage('Benutzerprofil gespeichert');
+		}
   }
 
   return true;
 }
 
-$(document).ready(function () {
+function setUserProfileForm(userProfile) {
 	"use strict";
-  var userProfile = getUserProfile();
 
-  $("#profileNickname").val(userProfile.nickname);
+	$("#profileNickname").val(userProfile.nickname);
   $("#profileEmail").val(userProfile.email);
   $("#profileUploadToken").val(userProfile.uploadToken);
-  if (userProfile.photoOwner || userProfile.photoOwner === "true") {
-    $("#profilePhotoOwner").attr('checked','checked');
-  }
-  if (userProfile.anonymous || userProfile.anonymous === "true") {
-    $("#profileAnonymous").attr('checked','checked');
-  }
+  $("#profilePhotoOwner").prop('checked',(userProfile.photoOwner || userProfile.photoOwner === "true"));
+  $("#profileAnonymous").prop('checked',(userProfile.anonymous || userProfile.anonymous === "true"));
   $("#profileLink").val(userProfile.link);
-  if (userProfile.cc0 || userProfile.cc0 === "true") {
-    $("#profileCc0").attr('checked','checked');
-  }
+  $("#profileCc0").prop('checked',(userProfile.cc0 || userProfile.cc0 === "true"));
+
+	if (loggedIn) {
+		$("#saveProfile").html("Speichern");
+	} else {
+		$("#saveProfile").html("Registrieren");
+	}
+}
+
+$(document).ready(function () {
+	"use strict";
+
+  var userProfile = getUserProfile();
+	setUserProfileForm(userProfile);
 
   var showPoints = getBoolFromLocalStorage("showPoints", false);
   if (showPoints) {
@@ -82,5 +217,7 @@ $(document).ready(function () {
   } else {
     $("#togglePoints").toggleClass("fa-toggle-off");
   }
+
+	login(userProfile, true);
 
 });
