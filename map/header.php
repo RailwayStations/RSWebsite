d1c61cc4fd7aeaec7f7918a5e508a23f22c2c653
Ben Antony <antony@synyx.de>
2019-12-17 23:49:05 +0100

    Initial setup for i18n

--- map/header.php
+++ map/header.php
@@ -1,3 +1,11 @@
+<?php
+require_once '../vendor/philipp15b/php-i18n/i18n.class.php';
+$i18n = new i18n();
+$i18n->setCachePath('../i18n/langcache');
+$i18n->setFilePath('../i18n/lang/lang_{LANGUAGE}.ini');
+$i18n->init();
+
+?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
