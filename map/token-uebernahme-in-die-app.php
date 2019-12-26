6172c45f230e52c1cb4bcf7564ce6175f0ffcae2
Ben Antony <antony@synyx.de>
2019-12-26 13:48:59 +0100

    Replace tabs with spaces

--- map/token-uebernahme-in-die-app.php
+++ map/token-uebernahme-in-die-app.php
@@ -19,20 +19,20 @@ navbar();
 
 <main role="main" class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content">
 
-	<div class="alert alert-success" role="alert">
-	  <h4 class="alert-heading"><?php echo $info; ?></b></h4>
-	  <p><?php echo $mobileInfo; ?></p>
-	  <hr>
-	  <p class="mb-0"><?php echo $happy; ?></p>
-	</div>
+    <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading"><?php echo $info; ?></b></h4>
+        <p><?php echo $mobileInfo; ?></p>
+        <hr>
+        <p class="mb-0"><?php echo $happy; ?></p>
+    </div>
 
 </main>
 
-		<script src="js/jquery-3.4.1.min.js"></script>
-		<script src="js/popper.min.js"></script>
-		<script src="bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js"></script>
 
-		<script src="js/common.js"></script>
-		<script src="js/updatePassword.js"></script>
-  </body>
+<script src="js/common.js"></script>
+<script src="js/updatePassword.js"></script>
+</body>
 </html>
