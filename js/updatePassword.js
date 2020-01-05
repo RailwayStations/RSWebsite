1db708303d8e6e6bb4de3ab1bacb2e1c33db5930
Peter Storch <peter.storch@gmail.com>
2020-01-05 17:36:27 +0100

    npm run format

--- js/updatePassword.js
+++ js/updatePassword.js
@@ -1,13 +1,13 @@
 import $ from "jquery";
 import "bootstrap";
-import { getQueryParameter, setUserProfile } from './common';
+import { getQueryParameter, setUserProfile } from "./common";
 
 $(document).ready(function() {
-    const vars = getQueryParameter();
-    const uploadToken = vars.uploadToken;
-    const userProfile = getUserProfile();
+  const vars = getQueryParameter();
+  const uploadToken = vars.uploadToken;
+  const userProfile = getUserProfile();
 
-    if (uploadToken && uploadToken.length > 0) {
+  if (uploadToken && uploadToken.length > 0) {
     userProfile.password = uploadToken;
     delete userProfile.uploadToken;
     setUserProfile(userProfile);
