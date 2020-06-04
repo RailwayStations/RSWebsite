de4c9709a39e48cc80df1aee01afcbb88886366a
Peter Storch <peter.storch@gmail.com>
2020-06-04 21:14:28 +0200

    add twitter #bahnhofsfoto link

--- php/navbar.php
+++ php/navbar.php
@@ -49,6 +49,11 @@
                        aria-label="<?php echo L::navbar_privacyPolicy; ?>"
                        title="<?php echo L::navbar_privacyPolicy; ?>"><em class="fas fa-shield-alt"></em></a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="https://twitter.com/search?q=%23bahnhofsfoto" rel="noopener"
+                       aria-label="<?php echo L::navbar_twitter; ?>"
+                       title="<?php echo L::navbar_twitter; ?>"><em class="fab fa-twitter"></em></a>
+                </li>
                 <?php if ($suffixNavItems !== null) {
                     echo $suffixNavItems;
                 } ?>
