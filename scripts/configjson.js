f9182f476e5a599bcbaab98e752aab70bbec0dda
Peter Storch <peter.storch@tuta.io>
2023-04-12 10:21:14 +0200

    Revert config json for station.php

---
+++ scripts/configjson.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+require('dotenv').config()
+
+const writeToFile = function (iniObj) {
+  const outputDirectory = "map/json";
+  if (!fs.existsSync(outputDirectory)) {
+    fs.mkdirSync(outputDirectory, true);
+  }
+
+  const iniFile = outputDirectory + "/config.json";
+  fs.writeFile(iniFile, iniObj, function (err) {
+    if (err) throw err;
+    console.log(`Saved ${iniFile}!`);
+  });
+};
+
+const main = function () {
+  console.log(`API_URL: ${process.env.API_URL}`);
+  const iniObj = fs.readFileSync(`config-template.json`, "utf-8");
+  writeToFile(
+    iniObj.replace("__API_URL__", process.env.API_URL)
+  );
+};
+main();
